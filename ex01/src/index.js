d68629cba29e97fe27d5d793c12efbfcf2132178
timurseta <timur_seta@hotmail.com>
2021-10-04 10:30:35 +0200

    final changes

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,5 +1,5 @@
-var i, c, capslock = 0, shift = 0;
-
+var i, c, caps = 0, shift = 0;
+    
 function init() {
     i = document.getElementById('keyboard_buttons').getElementsByTagName('button');
     for(c = 0; c < i.length; c++) {
@@ -15,30 +15,34 @@ function makeClickHandler(c) {
             document.getElementById('text_area').value = document.getElementById('text_area').value.slice(0, -1);
         } else if(i[c].id === 'ok') {
             document.getElementById('text_area').value += '\n';
-        } else if(i[c].id === 'capslock') {
-            if(capslock === 0) {
-                capslock = 1;
+        }  else if(i[c].id === 'capslock') {
+            if(caps === 0) {
+                caps = 1;
             } else {
-                capslock = 0;
+                caps = 0;
             }
         } else if(i[c].id === 'shift') {
             shift = 1;
-        } else if(capslock === 1) {
+        } else if(caps === 1) {
             if(shift === 1) {
                 document.getElementById('text_area').value += this.value.toLowerCase();
                 shift = 0;
             } else {
-            document.getElementById('text_area').value += this.value.toUpperCase();
+                document.getElementById('text_area').value += this.value.toUpperCase();
             }
-        } else {
-            if( shift === 1) {
-                document.getElementById('text_area').value += this.value.tuUpperCase();
+        } else {   
+            if(shift === 1) {
+                document.getElementById('text_area').value += this.value.toUpperCase();
                 shift = 0;
             } else {
-            document.getElementById('text_area').value += this.value.toLowerCase();
+                document.getElementById('text_area').value += this.value.toLowerCase();
             }
        }
     };
+}
+
+function showAlert() {
+    alert(document.getElementById('text_area').value);
 }
 
 window.addEventListener?
